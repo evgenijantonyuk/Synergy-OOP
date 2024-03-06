de3f4a9844f0ebe2b1694246c74b9d09cc1c3a12
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-03-06 18:16:50 +0700

    add homework classes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// HomeWork 11.1
+
+//  создаем конструктор класса Employee
+class Employee {
+    constructor(name, surname, rate,workingDays) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.workingDays = workingDays;
+        this.salary = this.rate * this.workingDays;  // вычисление заработной платы
+        
+        if (rate > 0 && workingDays > 0) {
+            console.log(`Вы трудитесь ${workingDays} дня(ей) и заработали ${this.salary}, при ставке ${rate}/день!`)
+        } else {
+            console.log('Вы ничего не заработали!')
+        }
+    }
+}
+// свойства объекта Employee
+let employee = new Employee('Ivan', 'Petrov', 300, 0)
+// расчет заработной платы SALARY - наследуется от родительского класса Employee
+
+// свойства объекта Employee1
+let employee1 = new Employee ('Olga', 'Smirnova', 1500, 3)
+//     расчет заработной платы SALARY - наследуется от родительского класса Employee
+
+console.log(employee) // показываем все свойства объекта
+console.log(employee1)  // показываем все свойства объекта-1
+
+// ============================================== Lesson ====================================================
 // объявление одиночного объекта
 // let book = {
 //     title: 'Anna Karenina',
@@ -44,56 +74,58 @@
 // console.log(typeof BookClass);
 
 // приватные свойства классов
-class User {
-    #password   // приватное свойство
-    constructor(email, password) {
-        this.email = email;
-        this.#password = password;
-    }
-    
-    login(email, password) {
-        if (email === this.email && password === this.#password) {
-            console.log('Login Successfully');
-        } else {
-            console.log('Authentication Failed!');
-        }
-    }
-    
-    resetPassword(newPassword) {
-        this.#password = newPassword;
-    }
-    
-    logout() {
-        console.log('Logout Successfully!')
-    }
-}
+// class User {
+//     #password   // приватное свойство
+//     constructor(email, password) {
+//         this.email = email;
+//         this.#password = password;
+//     }
+//
+//     login(email, password) {
+//         if (email === this.email && password === this.#password) {
+//             console.log('Login Successfully');
+//         } else {
+//             console.log('Authentication Failed!');
+//         }
+//     }
+//
+//     resetPassword(newPassword) {
+//         this.#password = newPassword;
+//     }
+//
+//     logout() {
+//         console.log('Logout Successfully!')
+//     }
+// }
+//
+// let user = new User('user@mail.com', 'password');
+// user.login('email', 'password');
+//
+// console.log(user.email);
+// console.log(user.password);
+//
+// user.resetPassword('new_password');
+// user.login('user@mail.com', 'password');
+// user.login('user@mail.com', 'new_password');
 
-let user = new User('user@mail.com', 'password');
-user.login('email', 'password');
+//  наследование
+// class Author extends User {
+//     #numOfPost;
+//     constructor(email, password) {
+//         super(email, password);
+//         this.#numOfPost = 0;
+//     }
+//     createPost(content) {
+//         console.log(content)
+//         this.#numOfPost++;
+//     }
+//     getNumOfPost() {
+//         return this.#numOfPost
+//     }
+// };
+// let user1 = new Author('user@mail.com', 'password');
+// user1.login('user1@mail.com', 'password');
+// user1.createPost('L.Tolstoy is the Russian author');
+// console.log(user1.getNumOfPost());
 
-console.log(user.email);
-console.log(user.password);
 
-user.resetPassword('new_password');
-user.login('user@mail.com', 'password');
-user.login('user@mail.com', 'new_password');
-
-//  раследование
-class Author extends User {
-    #numOfPost;
-    constructor(email, password) {
-        super(email, password);
-        this.#numOfPost = 0;
-    }
-    createPost(content) {
-        console.log(content)
-        this.#numOfPost++;
-    }
-    getNumOfPost() {
-        return this.#numOfPost
-    }
-};
-let user1 = new Author('user@mail.com', 'password');
-user1.login('user1@mail.com', 'password');
-user1.createPost('L.Tolstoy is the Russian author');
-console.log(user1.getNumOfPost());
